b5dc5eba1227e768a5898ec3f629af9c658af624
Sravanthi <sravanthigurgubelli@gmail.com>
2024-10-01 23:35:48 +0530

    Updated arrays

--- ArraysDemo/arry2.java
+++ ArraysDemo/arry2.java
@@ -1,5 +1,22 @@
 package ArraysDemo;
 
+import java.util.Scanner;
+
 public class arry2 {
-  
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("enter the number :");
+    int num = sc.nextInt();
+    int[] arr = new int[num];
+    System.out.println("enter" + num + "each");
+    for (int i = 0; i < num; i++) {
+      arr[i] = sc.nextInt();
+    }
+    System.out.println("your numbers are:");
+    for (int i = 0; i < num; i++) {
+      System.out.println(arr[i]);
+    }
+  }
 }
